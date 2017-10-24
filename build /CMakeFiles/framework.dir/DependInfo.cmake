f8608d82b811168502b7fef62f697d2d24e399ba
Zzrop <Silbendrechsler@gmx.de>
2017-10-24 16:14:02 +0200

    Is it working?

---
+++ build /CMakeFiles/framework.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/external/tinyobjloader-aa07206/tiny_obj_loader.cc" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/external/tinyobjloader-aa07206/tiny_obj_loader.cc.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/application.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/application.cpp.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/launcher.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/launcher.cpp.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/model.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/model.cpp.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/model_loader.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/model_loader.cpp.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/shader_loader.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/shader_loader.cpp.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/texture_loader.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/texture_loader.cpp.o"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/framework/source/utils.cpp" "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /CMakeFiles/framework.dir/framework/source/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLBINDING_STATIC_DEFINE"
+  "SYSTEM_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/glm-0.9.6.3"
+  "../external/stb_image-2.0.6"
+  "../external/tinyobjloader-aa07206"
+  "../external/glfw-3.1.1/include"
+  "../framework/include"
+  "../application/include"
+  "../external/glbinding-2.1.1/source/glbinding/include"
+  "external/glbinding-2.1.1/source/glbinding/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/DependInfo.cmake"
+  "/home/max/Dokumente/Computer_Grafik/OpenGLFramework/build /external/glfw-3.1.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
